374ffe45bfa00a75920f20ac094095e4f8c4ee26
TheSkyScout <antonio.wilczynski2006@gmail.com>
2022-12-26 09:54:57 +0100

    edit

--- cogs/AutoModeration.py
+++ cogs/AutoModeration.py
@@ -6,13 +6,13 @@ class AutoModeration(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
     @commands.Cog.listener()
     async def on_message(self, message: nextcord.Message):
         author: nextcord.Member = message.author
         BlackList = ["hurensohn", "nuttensohn", "arschloch", "neger", "nigger", "negger", "hs", "hillbilly",
                      "arschgesicht", "dummkopf", "eierkopf", "hackfresse", "hurenkind", "schwanzlutscher",
-                     "schwachkopf", "sackratte", "vollpfosten", "wichser", "wixer", "wixxer", "wickser", "wikser"]
+                     "schwachkopf", "sackratte", "vollpfosten", "wichser", "wixer", "wixxer", "wickser", "wikser",
+                     "mooz"]
         for word in BlackList:
             if message.content.lower().__contains__(word):
                 embed = nextcord.Embed(title='So nicht', description='Bitte verwende Solche Worte nicht!',
